871f6e45b8291d6b05ac61dac5613c0293697ea0
AgrawalTrisha <agrawaltrisha@gmail.com>
2021-03-06 10:36:42 -0600

    created game.js

---
+++ game.js
@@ -0,0 +1,14 @@
+var config = {
+  width: 360,
+  height: 272,
+  scale: {
+    parent: 'yourgamediv',
+    mode: Phaser.Scale.NONE,
+    zoom: 2.5,
+  },
+  backgroundColor: 0xDA5E53,
+  scene: [],
+	pixelArt: true,
+}
+
+var game = new Phaser.Game(config);
